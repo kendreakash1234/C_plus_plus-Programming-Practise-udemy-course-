ee9b33bce7b9e014f761a8a5fb7ad69af3589c2b
adkendre <kendreakash1234@gmail.com>
2025-08-09 19:03:45 +0530

    Basic of C++

---
+++ BasicOfC++/cin&cout.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{   
+    int a;
+    cout<<"Enter the value of a = ";
+    cin>>a;
+
+    int b,c;
+    cout<<"Enter the value of b & c = ";
+    cin>>b>>c;
+    cout<<"You have entered a = "<<a<<" b = "<<b<<" c = "<<c<<endl;
+    
+    return 0;
+}
