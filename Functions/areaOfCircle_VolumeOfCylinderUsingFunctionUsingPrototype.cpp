89935a97a3209fd262349b39c42f1bf74ef57e16
adkendre <kendreakash1234@gmail.com>
2025-09-02 12:12:14 +0530

    function overloading

---
+++ Functions/areaOfCircle_VolumeOfCylinderUsingFunctionUsingPrototype.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+const double pi=3.14159;
+
+void area_of_circle();
+double calc_area_circle(double );
+double calc_volume_of_cylinder(double , double );
+void volume_cylinder();
+
+int main()
+{
+    area_of_circle();
+    volume_cylinder();
+
+    return 0;
+}
+
+double calc_area_circle(double radius)
+{
+    return pi*radius*radius;
+}
+
+void area_of_circle()
+{
+    double radius{};
+    cout<<"Enter the radius : ";
+    cin>>radius;
+    cout<<"Area of Circle whose radius is "<<radius<<" is "<<calc_area_circle(radius)<<endl;
+}
+
+double calc_volume_of_cylinder(double radius, double height){
+    return pi*radius*radius*height;    
+}
+
+void volume_cylinder()
+{
+    double radius{}, height{};
+    cout<<"Enter the radius and height of cylinder: ";
+    cin>>radius>>height;
+    cout<<"Volume of cylinder is "<<calc_volume_of_cylinder(radius, height)<<endl;
+}
