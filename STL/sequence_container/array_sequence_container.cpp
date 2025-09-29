8814de561ad40a0792a4c369ccd73ef4ac951f05
kendreakash1234 <kendreakash1234@gmail.com>
2025-09-29 09:47:59 +0530

    sequence constainers

--- STL/sequence_container/array_sequence_container.cpp
+++ STL/sequence_container/array_sequence_container.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<array>
+#include<algorithm>
 using namespace std;
 
 void display(array<int, 5> &arr){
@@ -17,26 +18,26 @@ int main()
     cout<<"size of arrays"<<endl;
     cout<<"size of array 1 = "<<arr1.size()<<endl;
     cout<<"size of array 2 = "<<arr2.size()<<endl;
-    
+
     arr1.at(0) = 100;
     arr1[1] = 2000;
     display(arr1);
     cout<<"front and back of array"<<endl;
     cout<<"front of arr1 "<<arr1.front()<<endl;
     cout<<"back of arr1 "<<arr1.back()<<endl;
-    
+
     arr2.fill(4);
     display(arr2);
     cout<<"After swapping"<<endl;
     arr1.swap(arr2);
     display(arr1);
     display(arr2);
-    
+
     int *ptr = arr2.data();
     cout<<"Address of arr2 "<<ptr<<endl;
     cout<<"After sorting"<<endl;
 
-    cout<<"minimum element in array "<<min_elelemt(arr2.begin(), arr2.end());
-    
+    cout<<"minimum element in array "<<std::min_element(arr2.begin(), arr2.end());
+
     return 0;
 }
